ceb33af1c491ff75cf8259bb030b440423e02691
nikhil-kulkarni <nikhil896@gmail.com>
2016-01-02 11:01:28 -0500

    User generator

---
+++ Mock Data/generator.py
@@ -0,0 +1,29 @@
+import openpyxl
+
+wb = openpyxl.load_workbook('MockData.xlsx')
+sheet = wb.get_sheet_by_name('Sheet1')
+print sheet['H2'].value
+for i in range(1, 66):
+	insertPref = "INSERT INTO User"
+	valuesPref = "VALUES ("
+	for c in range(0, 12):
+		ch = chr(ord('A') + c)
+		col = ch + str(i)
+		# if sheet[col].value != None:
+		# print sheet[col].value
+		if c != 11:
+			if sheet[col].value == None:
+				valuesPref += "NULL" + ","
+			else:
+				valuesPref += str(sheet[col].value) + ","
+		else:
+			if sheet[col].value == None:
+				valuesPref += "NULL" + ");"
+			else:
+				valuesPref += str(sheet[col].value) + ");"
+	print insertPref
+	print valuesPref
+	valuesPref = "VALUES ("
+
+
+
